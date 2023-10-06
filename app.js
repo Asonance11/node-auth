9b912a349c15dbfdd174d3bc0576cdce186c079e
Oluwadabira <adigunsamuelaso@gmail.com>
2023-10-06 08:35:19 +0100

    addition of passport js

--- app.js
+++ app.js
@@ -25,6 +25,35 @@ app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 
 app.use(session({ secret: 'cats', resave: false, saveUninitialized: true }));
+
+passport.use(
+	new localStrategy(async (username, password, done) => {
+		try {
+			const user = await User.findOne({ username: username });
+			if (!user) {
+				return done(null, false, { message: 'Incorrect username' });
+			}
+			if (user.password !== password) {
+				return done(null, false, { message: 'Incorrect password' });
+			}
+			return done(null, user);
+		} catch (error) {
+			return done(error);
+		}
+	})
+);
+
+passport.serializeUser((user, done) => done(null, user.id));
+
+passport.deserializeUser(async (id, done) => {
+	try {
+		const user = await User.findById(id);
+		done(null, user);
+	} catch (error) {
+		done(error);
+	}
+});
+
 app.use(passport.initialize());
 app.use(passport.session());
 app.use(express.urlencoded({ extended: false }));
